d89b264658139e7c9e2ac37cc0e432a2a38f1bf9
unknown <tom@mrs.com.vn>
2019-09-25 23:22:21 +0700

    Modified API

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -4,40 +4,26 @@ import Loading from "../../components/loading";
 import moment from "moment";
 
 class PostDetail extends Component {
-  static async getInitialProps({ query }) {
-    const { slug } = query;
-    const { data } = await Axios.get(
-      `http://localhost:8000/api/posts/slug?slug=${slug}`
-    );
+  // static async getInitialProps({ query }) {
+  //   const { slug } = query;
+  //   const { data } = await Axios.get(
+  //     `http://localhost:8000/api/posts/slug?slug=${slug}`
+  //   );
 
-    return {
-      post: data
-    };
-  }
-  //  static async getInitialProps({query}) {
-  //   const slug = query.slug;
-  //   await Axios({
-  //     method: "GET",
-  //     url: `http://localhost:8000/api/posts/slug?slug=${slug}`
-  //   })
-  //     .then(res => {
-  //       this.setState({
-  //         detailNews: res.data
-  //       });
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     });
-  // };
+  //   return {
+  //     post: data
+  //   };
+  // }
+  
   render() {
     return (
       <Fragment>
-        <div style={{ fontSize: "4rem" }}>
+        {/* <div style={{ fontSize: "4rem" }}>
           ddd title: {this.props.post.title}
           date:{" "}
           {moment(this.props.post.createdOn).format("MMMM Do YYYY, h:mm:ss a")}
           createdAt: {this.props.post.createdAt}
-        </div>
+        </div> */}
       </Fragment>
     );
   }
